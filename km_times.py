4a67762413e4f0535e848881b04feba10bf0aea8
Ralf Holly <ralf.holly@approxion.com>
2019-07-01 13:17:15 +0200

    Improved rounding of secs

--- km_times.py
+++ km_times.py
@@ -4,15 +4,25 @@ import math
 import sys
 
 
-def hoursToHHMMSS(hours):
+def hours_to_hhmmss(hours):
     hh = math.trunc(hours)
     hours -= hh
     mm = math.trunc(hours * 60.0)
     hours -= mm / 60.0
-    ss = round(hours * 60.0 * 60.0)
+    secs = hours * 60.0 * 60.0
+    ss = math.trunc(hours * 60.0 * 60.0)
+    if secs - ss >= 0.5:
+        ss += 1
+        if ss >= 60:
+            ss = 0
+            mm += 1
+            if mm >= 60:
+                mm = 0
+                hh += 1
     return (hh, mm, ss)
 
-if __name__ == '__main__':
+
+def main():
     if len(sys.argv) != 1 + 1:
         print("Usage:", sys.argv[0], "<time for 1 km mm:ss>")
         sys.exit(1)
@@ -22,5 +32,9 @@ if __name__ == '__main__':
 
     for km in range(1, 43):
         hours = km * km_time
-        (hh, mm, ss) = hoursToHHMMSS(hours)
+        (hh, mm, ss) = hours_to_hhmmss(hours)
         print("km %2d   %02d:%02d:%02d" % (km, hh, mm, ss))
+
+
+if __name__ == '__main__':
+    main()
